93dad877b54d26c58774f07ec7902b377b6ae919
Garth Gilmour <garth.gilmour@jetbrains.com>
2024-04-22 10:22:00 +0100

    Add sample project for first blog

---
+++ kotlin-dataframe-step-by-step/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package data.frame.demos
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val fileName = "using-dataframes.ipynb"
+    val message = """
+        This project uses Kotlin Notebooks,
+        please see '$fileName'
+    """.trimIndent()
+
+    println(message)
+}
